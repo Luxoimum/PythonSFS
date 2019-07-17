d8cf45a70f8fa2620268838a78e3d6588dcf3b7d
Federico Pereyra López <federico.pereyra@upm.es>
2019-07-17 20:22:28 +0200

    cambios en el servidor

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PythonSFS
 Python Simple File System es una pequeña API que funciona como servicio interno de carga y descarga de ficheros en una red local. Para ello se utiliza la librería Flask.
+
+### Version 1.0:
+-   GET de todas las imagenes
+      
+        localhost:5000/images
+        
+-   GET de una imagen
+
+        localhost:5000/images/<name>
+        
+### Funcionalidad por hacer
+-   Crear rama POST para una imagen
+-   El GET de todas las imágenes no diferencia entre ficheros y carpetas
+-   Contemplar distitnas extensiones de fichero (jpg, jpeg, png, swf, etc)
+-   Crear ramas para navegar entre carpetas
--- src/controllers/files_controller.py
+++ src/controllers/files_controller.py
@@ -1,35 +1,52 @@
 from flask import Flask, send_file
 from flask_restful import Resource, Api
 from src.services.upload import UploadFile as uf
-import json
+import json, os.path
 
 
 app = Flask(__name__)
 api = Api(app)
+SFSDir = '../../SFSDir/'
+path = 'localhost:5000/images/'
 
 
-# Upload an image into the server
+class ImagesOperations (Resource):
+    def get(self, name):
+        if os.path.exists(SFSDir + name + '.png'):
+            return send_file(SFSDir + name + '.png')
+        elif os.path.exists(SFSDir + name + '.jpg'):
+            return send_file(SFSDir + name + '.jpg')
+        else:
+            return 'El fichero no existe );', 404
 
-
-class Upload (Resource):
     def post(self, img, name):
         uf.upload_file(json.loads(img), name)
         return '', 201
 
 
-# Download an image
+class Images (Resource):
+    def get(self):
+        files = []
+        res = []
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(SFSDir):
+            for file in f:
+                files.append(path + os.path.join(r, file))
 
+        for f in files:
+            fil = f.replace(SFSDir, '')
+            fil = fil.replace('.jpg', '')
+            fil = fil.replace('.png', '')
+            res.append(fil)
 
-class Download (Resource):
-    def get(self, name):
-        return send_file('../../SFSDir/'+name)
+        return res
 
 
 # Add resources with uri's associated to library
 
 
-api.add_resource(Upload, '/upload/<name>')
-api.add_resource(Download, '/download/<name>')
+api.add_resource(Images, '/images')
+api.add_resource(ImagesOperations, '/images/<name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
